7db497d379f1e162b2bf049e3fe3ab65f4337374
Adajli <ajli@smith.edu>
2024-11-01 13:09:10 -0400

    Modified statement for addBuilding of the library.

--- CampusMap.java
+++ CampusMap.java
@@ -47,14 +47,17 @@ public class CampusMap {
         CampusMap myMap = new CampusMap();
         myMap.addBuilding(new Building("Ford Hall", "100 Green Street Northampton, MA 01063", 4, true));
         myMap.addBuilding(new Building("Bass Hall", "4 Tyler Court Northampton, MA 01063", 4, true));
-        myMap.addBuilding((Building) new Library("Neilson Library", "7 Neilson Drive, Northampton, MA 01063", 4, true));
+        myMap.addBuilding(new Library("Neilson Library", "7 Neilson Drive, Northampton, MA 01063", 4, true));
         for (int i = 0; i < myMap.buildings.size(); i++) {
             if (myMap.buildings.get(i) instanceof Library) {
                 Library library = (Library) myMap.buildings.get(i);
                 library.addTitle("The Art of War");
                 library.addTitle("Astronomy");
                 System.out.println(library.isAvailable("Astronomy"));
-
+            }
+            if (myMap.buildings.get(i) instanceof Cafe) {
+                Cafe cafe = (Cafe) myMap.buildings.get(i);
+                cafe.sellCoffee(20, 0, 1);
             }
         }
         System.out.println(myMap);
