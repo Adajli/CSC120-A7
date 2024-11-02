07603fc6392562f5eaaa940a2703fc757de4ffe2
Adajli <ajli@smith.edu>
2024-11-01 22:02:15 -0400

    Modified statements to test the overloaded methods.

--- CampusMap.java
+++ CampusMap.java
@@ -48,18 +48,12 @@ public class CampusMap {
         myMap.addBuilding(new Building("Ford Hall", "100 Green Street Northampton, MA 01063", 4, true));
         myMap.addBuilding(new Building("Bass Hall", "4 Tyler Court Northampton, MA 01063", 4, true));
         myMap.addBuilding(new Library("Neilson Library", "7 Neilson Drive, Northampton, MA 01063", 4, true));
-        for (int i = 0; i < myMap.buildings.size(); i++) {
-            if (myMap.buildings.get(i) instanceof Library) {
-                Library library = (Library) myMap.buildings.get(i);
-                library.addTitle("The Art of War");
-                library.addTitle("Astronomy");
-                System.out.println(library.isAvailable("Astronomy"));
-            }
-            if (myMap.buildings.get(i) instanceof Cafe) {
-                Cafe cafe = (Cafe) myMap.buildings.get(i);
-                cafe.sellCoffee(20, 0, 1);
-            }
-        }
+        myMap.addBuilding(new House("Talbot House", "25 Prospect St, Northampton, MA 01063", 5, false, true));
+        myMap.addBuilding(new Cafe("Campus Center Cafe", "100 Elm St, Northampton, MA 01063"));
+        myMap.buildings.get(2).enter();
+        myMap.buildings.get(3).enter();
+        myMap.buildings.get(4).enter();
+
         System.out.println(myMap);
     }
 
